612349b6207a1c2dace44abf6b0b47ca30ab1b9c
roispro05 <coursesroispro21@gmail.com>
2025-01-09 14:05:54 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from app import app
 
 if __name__ == "__main__":
-    app.run()
-
+    import os
+    port = int(os.getenv("PORT", 5000))  # Use Render's dynamic PORT or default to 5000
+    app.run(host="0.0.0.0", port=port)
