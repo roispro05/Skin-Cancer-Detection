90317ac32ad9f32b255ce5f2bd154fc7e389f4c4
roispro05 <coursesroispro21@gmail.com>
2025-01-09 14:02:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -141,4 +141,8 @@ def index():
         })
 
     return render_template('index.html')
+if __name__ == "__main__":
+    import os
+    port = int(os.getenv("PORT", 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port)
 
